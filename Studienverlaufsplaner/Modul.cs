74a83d2cbf777d056f9e7b8244e70403a74f787c
anhieb <a.hiebert@gmx.de>
2021-08-11 23:25:57 +0200

    Projektdateien hinzufügen.

---
+++ Studienverlaufsplaner/Modul.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studienverlaufsplaner
+{
+    class Modul
+    {
+        public string name;
+        public bool bestanden;
+        public double note;
+        public double cp;
+        public double gewichtung;
+    }
+}
