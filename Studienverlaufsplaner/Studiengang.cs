cb254775dc81ecd94a0b417839aa19e276856fda
anhieb <a.hiebert@gmx.de>
2021-08-24 15:00:03 +0200

    ETR Module eingetragen

--- Studienverlaufsplaner/Studiengang.cs
+++ Studienverlaufsplaner/Studiengang.cs
@@ -74,9 +74,52 @@ namespace Studienverlaufsplaner
             module.Add(new Modul("Ausgewählte anwendungen und praxisfelder",4 , 1));
             module.Add(new Modul("Studienschwerpunkt 3", 8, 1));
 
+        }
 
+        private void loadETRModuls()
+        {
+            //Semester 1:
+            module.Add(new Modul("Nachhaltige Ressourcenwirtschaft und Energieversorgung",4, 1));
+            module.Add(new Modul("Grundlagen Mathematik und Mechanik",10, 1));
+            module.Add(new Modul("Naturwissenschaftliche Grundlagen",9, 1));
+            module.Add(new Modul("Unternehmensführung 1",3.5, 1));
+            module.Add(new Modul("Steuerungskompetenzen 1",3.5, 1));
 
+            //Semester 2:
+            module.Add(new Modul("Grundlagen Mathematik und Elektrotechnik",9, 1));
+            module.Add(new Modul("Grundlagen Energie- und Stoffumwaldung",8, 1));
+            module.Add(new Modul("Grundlagen Werkstoffe und Mechanik",6, 1));
+            module.Add(new Modul("Unternehmensführung 2",3.5, 1));
+            module.Add(new Modul("Steuerungskompetenzen 2",3.5, 1));
+
+            //Semester 3:
+            module.Add(new Modul("Mathematik und Elektromaschinen",10, 1));
+            module.Add(new Modul("Wärme- und Strömungstechnik",12, 1));
+            module.Add(new Modul("Konstruktionslehre",5, 1));
+            module.Add(new Modul("Steuerungskompetenzen 3",6, 1));
+
+            //Semester 4:
+            module.Add(new Modul("Energiesysteem: Infrastruktur und Handelsmärkte",10, 1));
+            module.Add(new Modul("Energieprozesstechnik",10, 1));
+            module.Add(new Modul("Studienschwerpunkte 1",6, 1));
+            module.Add(new Modul("Steuerungskompetenzen 3",4, 1));
+
+            //Semester 5:
+            module.Add(new Modul("Praxis-/ Auslandssemester",30, 1));
+
+            //Semester 6:
+            module.Add(new Modul("Projektarbeit einschließl. Projektseminar",16, 1));
+            module.Add(new Modul("Studienschwerpunkte 2a",6, 1));
+            module.Add(new Modul("Studienschwerpunkte 2b",5, 1));
+            module.Add(new Modul("Steuerungskompetenzen 4",4, 1));
+
+            //Semester 7:
+            module.Add(new Modul("Bachelorarbeit einschließl. Bachelorseminar",14, 1));
+            module.Add(new Modul("Studienschwerpunkte 3a",6, 1));
+            module.Add(new Modul("Studienschwerpunkte 3b",5, 1));
+            module.Add(new Modul("Produktgestaltung ",5, 1));
 
         }
+
     }
 }
